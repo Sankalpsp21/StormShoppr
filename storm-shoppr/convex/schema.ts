3e32f19aca03e7f2ff20c9a56b27a465ac8c8d94
Sankalp Patil <patilsa@oregonstate.edu>
2024-09-14 16:47:53 -0400

    modified schema

--- storm-shoppr/convex/schema.ts
+++ storm-shoppr/convex/schema.ts
@@ -2,12 +2,24 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+  products: defineTable({
+    id: v.string(),
+    name: v.string(),
+    description: v.optional(v.string()),
+    price: v.optional(v.number()),
+  })
+    .index("id", ["id"]),
+
   users: defineTable({
     // Unique identifier from the auth provider
     tokenIdentifier: v.string(),
     name: v.string(),
     username: v.string(),
     pictureUrl: v.string(),
+    latitude: v.optional(v.number()),
+    longitude: v.optional(v.number()),
+    products: v.optional(v.array(v.id("products"))),
+    autoOrder: v.optional(v.boolean()),
   })
     .index("tokenIdentifier", ["tokenIdentifier"])
     .index("username", ["username"])
