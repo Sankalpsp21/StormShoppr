7175a5af8a720e528c9b8299999987f785494827
anna belenko <66851299+annabelenko@users.noreply.github.com>
2024-09-14 22:43:52 -0400

    updated button styles

--- storm-shoppr/components/HeroSection.tsx
+++ storm-shoppr/components/HeroSection.tsx
@@ -1,45 +1,60 @@
 "use client";
 
-
+import '../app/globals.css'; 
 import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
 import React from "react";
 
-
 const HeroSection: React.FC = () => {
- return (
-   <div
-     className="h-screen flex items-center justify-center bg-cover bg-center"
-     style={{
-       backgroundImage: "url('/background.svg')"
-     }}
-   >
-     {/* Glassmorphism Container */}
-     <div className="backdrop-blur-md bg-white/30 border border-white/20 rounded-2xl pt-12 pb-12 w-full md:w-10/12 shadow-lg text-center">       
-      <div className="flex justify-center mb-6">
-        <img src="/icon.svg" alt="Icon" className="h-36 w-36" /> {/* Adjust size as needed */}
+  const stormText = "STORM"; // Initialize the word "STORM" as a variable
+
+  return (
+    <div
+      className="h-screen flex items-center justify-center bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/background.svg')"
+      }}
+    >
+      {/* Glassmorphism Container */}
+      <div className="backdrop-blur-md bg-white/30 border border-white/20 rounded-2xl pt-12 pb-12 w-full md:w-10/12 shadow-lg text-center">
+        <div className="flex justify-center mb-6">
+          <img src="/icon.svg" alt="Icon" className="h-36 w-36" /> {/* Adjust size as needed */}
+        </div>
+        <h1 className="text-5xl md:text-6xl lg:text-8xl font-bold mb-4 text-gray-700">
+          <div>
+            WELCOME TO <br />
+            <span
+              style={{
+                backgroundImage: "url('/clouds.avif')",
+                backgroundSize: 'cover',
+                backgroundClip: 'text',
+                WebkitBackgroundClip: 'text',
+                color: 'transparent',
+                fontWeight: 'bold',
+                fontStyle: 'italic', // Italicize the text
+              }}
+            >
+              {stormText} {/* Render the word "STORM" dynamically */}
+            </span>
+            <span className="italic"> SHOPPR</span>
+          </div>
+        </h1>
+
+        <div className="h-1 w-1/4 mx-auto my-8 bg-white rounded"></div>
+
+        <p className="text-lg md:text-2xl mb-6 text-gray-700">
+          Predict natural disasters <span className="font-bold">EARLY</span> and stay ahead by purchasing supplies!
+        </p>
+
+        <div className="flex justify-center">
+          <SignInButton mode="modal" afterSignInUrl="/form">
+            <button className="bg-[#ff8a6f] text-white text-lg font-bold py-5 px-20 rounded-full hover:bg-[#ee6b4c] hover:text-white">
+              Sign In
+            </button>
+          </SignInButton>
+        </div>
       </div>
-      <h1 className="text-5xl md:text-6xl lg:text-8xl font-bold mb-4 text-gray-700">
-         WELCOME TO <br></br><span className="italic">STORM SHOPPR</span>
-      </h1>
-
-
-       <div className="h-1 w-1/4 mx-auto my-8 bg-white rounded"></div>
-
-
-       <p className="text-lg md:text-2xl mb-6 text-gray-700">
-         Predict natural disasters <span className="font-bold">EARLY</span> and stay ahead by purchasing supplies!
-       </p>
-       <div className="flex justify-center">
-         <SignInButton mode="modal" afterSignInUrl="/form">
-           <button className="bg-[#ff8a6f] text-white text-lg font-bold py-5 px-20 rounded-full hover:bg-[#ee6b4c] hover:text-white">
-             Sign In
-           </button>
-         </SignInButton>
-       </div>
-     </div>
-   </div>
- );
+    </div>
+  );
 };
-
 
 export default HeroSection;
--- storm-shoppr/components/Nav.tsx
+++ storm-shoppr/components/Nav.tsx
@@ -2,68 +2,66 @@
 import { useConvexAuth } from "convex/react";
 import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
 
-
 const Nav = () => {
- const { isAuthenticated, isLoading } = useConvexAuth();
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
+  function SignInAndSignUpButtons() {
+    return (
+      <div className="flex gap-4">
+        {/* Loading Spinner */}
+        {isLoading && <p>Loading...</p>}
 
- function SignInAndSignUpButtons() {
-   return (
-     <div className="flex gap-4">
-       {/* Loading Spinner */}
-       {isLoading && <p>Loading...</p>}
-      
-       {/* If not authenticated and not loading, show Sign In and Sign Up buttons */}
-       {!isAuthenticated && !isLoading && (
-         <>
-           <SignInButton mode="modal" afterSignInUrl="/form">
-             <button className="bg-white text-gray-500 font-bold py-2 px-6 rounded-full hover:bg-gray-100">
-               Sign In
-             </button>
-           </SignInButton>
-           <SignUpButton mode="modal" afterSignUpUrl="/form">
-             <button className="bg-white text-gray-500 font-bold py-2 px-6 rounded-full hover:bg-gray-100">
-               Sign Up
-             </button>
-           </SignUpButton>
-         </>
-       )}
+        {/* If not authenticated and not loading, show Sign In and Sign Up buttons */}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal" afterSignInUrl="/form">
+              <button className="bg-[#ff8a6f] text-white font-bold py-2 px-8 rounded-full hover:bg-[#ee6b4c] hover:text-white">
+                Sign In
+              </button>
+            </SignInButton>
+            <SignUpButton mode="modal" afterSignUpUrl="/form">
+              <button className="bg-[#ff8a6f] text-white font-bold py-2 px-8 rounded-full hover:bg-[#ee6b4c] hover:text-white">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </>
+        )}
 
+        {/* If authenticated, show User Button */}
+        {isAuthenticated && !isLoading && (
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                userButtonAvatarBox: "w-6 h-6",
+                userButton: "bg-[#ff8a6f] text-white font-bold py-5 px-16 rounded-full hover:bg-[#ee6b4c] hover:text-white",
+              },
+            }}
+          />
+        )}
+      </div>
+    );
+  }
 
-       {/* If authenticated, show User Button */}
-       {isAuthenticated && !isLoading && (
-         <UserButton
-           afterSignOutUrl="/"
-           appearance={{
-             elements: {
-               userButtonAvatarBox: "w-8 h-8",
-               userButton: "bg-white text-gray-500 font-bold py-2 px-6 rounded-full hover:bg-gray-100",
-             },
-           }}
-         />
-       )}
-     </div>
-   );
- }
+  return (
+    <div
+      className={`fixed w-full flex justify-between items-center p-4 z-50 bg-white border border-white/20 rounded-lg shadow-lg ${
+        !isAuthenticated || isLoading
+          ? "backdrop-blur-md bg-white/30"
+          : "bg-white"
+      }`}
+    >
+      {/* Logo on the left */}
+      <div>
+        <img src="/logo.svg" alt="Logo" className="h-12" /> {/* Adjust height if necessary */}
+      </div>
 
-
- return (
-   <div className={`fixed w-full flex justify-between items-center p-4 z-50 bg-white border border-white/20 rounded-lg shadow-lg ${!isAuthenticated || isLoading
-   ? 'backdrop-blur-md bg-white/30'
-   : 'bg-white'}`}>
-     {/* Logo on the left */}
-     <div>
-       <img src="/logo.svg" alt="Logo" className="h-12" /> {/* Adjust height if necessary */}
-     </div>
-
-
-     {/* Sign In and Sign Up buttons on the right */}
-     <div className="flex gap-4 items-center">
-       <SignInAndSignUpButtons />
-     </div>
-   </div>
- );
+      {/* Sign In and Sign Up buttons on the right */}
+      <div className="flex gap-4 items-center">
+        <SignInAndSignUpButtons />
+      </div>
+    </div>
+  );
 };
-
 
 export default Nav;
