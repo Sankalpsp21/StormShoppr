446aa2854d939896cf9af8b2a0a703b1dd848fef
Potatoboy9999 <51728317+PotatoPresident@users.noreply.github.com>
2024-09-15 07:09:01 -0700

    Fix code

--- storm-shoppr/convex/createDoorDashDelivery.js
+++ storm-shoppr/convex/createDoorDashDelivery.js
@@ -7,7 +7,7 @@ const SIGNING_SECRET = process.env.DOORDASH_API_TOKEN;
 const KEY_ID = process.env.DOORDASH_API_TOKEN;
 
 exports.default = convex.asyncFunc(async ({ input }) => {
-  const { external_delivery_id = , pickup_address, pickup_business_name, pickup_phone_number, pickup_instructions, dropoff_address, dropoff_business_name, dropoff_phone_number, dropoff_instructions, order_value } = input;
+  const { external_delivery_id, pickup_address, pickup_business_name, pickup_phone_number, pickup_instructions, dropoff_address, dropoff_business_name, dropoff_phone_number, dropoff_instructions, order_value } = input;
 
   const body = JSON.stringify({
     external_delivery_id,
