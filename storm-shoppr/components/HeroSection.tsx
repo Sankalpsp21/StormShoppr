3451d6caa0fe2b659909c01fcdbf21563340ef83
anna belenko <66851299+annabelenko@users.noreply.github.com>
2024-09-14 21:31:03 -0400

    Fixed the sizing of container class

--- storm-shoppr/components/HeroSection.tsx
+++ storm-shoppr/components/HeroSection.tsx
@@ -14,21 +14,24 @@ const HeroSection: React.FC = () => {
      }}
    >
      {/* Glassmorphism Container */}
-     <div className="backdrop-blur-md bg-white/30 border border-white/20 rounded-3xl pt-24 pb-16 md:pt-52 md:pb-48 w-full md:w-10/12 shadow-lg text-center">
-       <h1 className="text-5xl md:text-6xl lg:text-8xl font-bold mb-4 text-gray-700">
+     <div className="backdrop-blur-md bg-white/30 border border-white/20 rounded-2xl pt-12 pb-12 w-full md:w-10/12 shadow-lg text-center">       
+      <div className="flex justify-center mb-6">
+        <img src="/icon.svg" alt="Icon" className="h-32 w-32" /> {/* Adjust size as needed */}
+      </div>
+      <h1 className="text-5xl md:text-6xl lg:text-8xl font-bold mb-4 text-gray-700">
          WELCOME TO <br></br><span className="italic">STORM SHOPPR</span>
-       </h1>
+      </h1>
 
 
        <div className="h-1 w-1/4 mx-auto my-8 bg-gray-300 rounded"></div>
 
 
-       <p className="text-lg md:text-3xl mb-6 text-gray-700">
+       <p className="text-lg md:text-2xl mb-6 text-gray-700">
          Predict natural disasters <span className="font-bold">EARLY</span> and stay ahead by purchasing supplies!
        </p>
        <div className="flex justify-center">
          <SignInButton mode="modal" afterSignInUrl="/form">
-           <button className="bg-white text-gray-500 text-2xl font-bold py-5 px-20 rounded-full hover:bg-[#ee6b4c] hover:text-white">
+           <button className="bg-[#ff8a6f] text-white text-lg font-bold py-5 px-20 rounded-full hover:bg-[#ee6b4c] hover:text-white">
              Sign In
            </button>
          </SignInButton>
